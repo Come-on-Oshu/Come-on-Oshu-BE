b8cc547e55ebfce77d063467b32e1998870d6295
Yujin_nKim <yujin.kimd@gmail.com>
2022-11-01 00:52:51 +0900

    chore : setting DB table - EventInfromation

--- oshuApp/models.py
+++ oshuApp/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class EventInformation(models.Model):
-    District = models.IntegerField()
-    EventName = models.TextField()
-    isFestival = models.IntegerField()
-    ImgUrl = models.TextField()
-    stDate = models.DateTimeField()
-    enDate = models.DateTimeField()
-    Organization = models.TextField()
-    Location = models.TextField()
-    WebsiteUrl = models.TextField()
-    
+    District = models.IntegerField() #행사 지역구
+    EventName = models.TextField() #행사 이름
+    isFestival = models.IntegerField() #행사 or 축제 여부
+    ImgUrl = models.TextField() #행사 포스터 이미지 url
+    stDate = models.DateTimeField() #행사 시작 날짜
+    enDate = models.DateTimeField() #행사 끝 날짜
+    Organization = models.TextField() #행사 주체 기관
+    Location = models.TextField() #행사 위치
+    WebsiteUrl = models.TextField() #행사 안내 웹사이트 url
